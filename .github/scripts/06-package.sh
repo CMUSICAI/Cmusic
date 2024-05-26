5599dfc81f0fee077db52ce631a4f6f9ad1f0815
Barrett Patterson <barrett@poolspots.com>
2024-05-26 13:18:03 -0400

    Updated packaging script

--- .github/scripts/06-package.sh
+++ .github/scripts/06-package.sh
@@ -14,26 +14,57 @@ echo "----------------------------------------"
 echo "Packaging Build for ${OS}"
 echo "----------------------------------------"
 
-mkdir -p ${WORKSPACE}/release
+RELEASE_DIR="$WORKSPACE/release"
+STAGE_DIR="$WORKSPACE/stage/cmusicai-v$VERSION"
 
-if [[ ${OS} == "windows" ]]; then
-    STRIPPROG="/usr/bin/x86_64-w64-mingw32-strip"
-    ${STRIPPROG} ${WORKSPACE}/src/cmusicaid.exe
-    ${STRIPPROG} ${WORKSPACE}/src/cmusicai-cli.exe
-    ${STRIPPROG} ${WORKSPACE}/src/qt/cmusicai-qt.exe
-    mv ${WORKSPACE}/src/cmusicaid.exe ${WORKSPACE}/release
-    mv ${WORKSPACE}/src/cmusicai-cli.exe ${WORKSPACE}/release
-    mv ${WORKSPACE}/src/qt/cmusicai-qt.exe ${WORKSPACE}/release
-    cd ${WORKSPACE}/release
-    zip -r ${WORKSPACE}/release/cmusicai-${VERSION}-win64.zip .
-elif [[ ${OS} == "linux" || ${OS} == "aarch64" || ${OS} == "arm32v7" ]]; then
-    cd ${WORKSPACE}/src
-    FILES="cmusicaid cmusicai-cli"
-    if [[ -f "qt/cmusicai-qt" ]]; then
-        FILES="${FILES} qt/cmusicai-qt"
+mkdir -p $RELEASE_DIR
+
+case $OS in
+  windows)
+    PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
+    make deploy
+    mv *-setup.exe ${STAGE_DIR}/cmusicai-v$VERSION-win64-setup-unsigned.exe
+
+    make install DESTDIR=${STAGE_DIR}/cmusicai-v$VERSION
+
+    cd ${STAGE_DIR}
+    mv cmusicai-v$VERSION/bin/*.dll cmusicai-v$VERSION/lib/
+    find . -name "lib*.la" -delete
+    find . -name "lib*.a" -delete
+    rm -rf cmusicai-v$VERSION/lib/pkgconfig
+
+    find cmusicai-v$VERSION/bin -type f -executable -exec x86_64-w64-mingw32-objcopy --only-keep-debug {} {}.dbg \; -exec x86_64-w64-mingw32-strip -s {} \; -exec x86_64-w64-mingw32-objcopy --add-gnu-debuglink={}.dbg {} \;
+
+    cp -rf ${WORKSPACE}/contrib/windeploy ${RELEASE_DIR}
+    mv ${STAGE_DIR}/cmusicai-v$VERSION-win64-setup-unsigned.exe ${RELEASE_DIR}
+
+    ;;
+  linux | arm32v7 | aarch64)
+    make install DESTDIR=${STAGE_DIR}/cmusicai-v$VERSION
+
+    cd ${STAGE_DIR}
+    find . -name "lib*.la" -delete
+    find . -name "lib*.a" -delete
+    rm -rf cmusicai-v$VERSION/lib/pkgconfig
+
+    if [[ -e ${STAGE_DIR}/cmusicai-v$VERSION/bin ]]; then
+        find cmusicai-v$VERSION/bin -type f -executable -exec ${WORKSPACE}/contrib/devtools/split-debug.sh {} {} {}.dbg \;
     fi
-    tar -czvf ${WORKSPACE}/release/cmusicai-${VERSION}-${OS}.tar.gz ${FILES}
-else
-    echo "Unsupported OS"
+    if [[ -e ${STAGE_DIR}/cmusicai-v$VERSION/lib ]]; then
+        find cmusicai-v$VERSION/lib -type f -exec ${WORKSPACE}/contrib/devtools/split-debug.sh {} {} {}.dbg \;
+    fi
+
+    ;;
+  *)
+    echo "Unknown OS: $OS"
     exit 1
-fi
+    ;;
+esac
+
+cd ${RELEASE_DIR}
+for i in cmusicai-v$VERSION-win64-setup-unsigned.exe cmusicai-v$VERSION-x86_64-linux-gnu.tar.gz cmusicai-v$VERSION-aarch64-linux-gnu.tar.gz cmusicai-v$VERSION-arm-linux-gnueabihf.tar.gz; do
+    if [[ -e ${i} ]]; then
+        md5sum ${i} >> ${i}.md5sum
+        sha256sum ${i} >> ${i}.sha256sum
+    fi
+done
