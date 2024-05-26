5f95e7af6bb04baaa6837dc686d2a98a7c476ccf
Barrett Patterson <barrett@poolspots.com>
2024-05-26 03:13:31 -0400

    Update 06-package.sh

--- .github/scripts/06-package.sh
+++ .github/scripts/06-package.sh
@@ -11,40 +11,25 @@ if [[ ! ${OS} || ! ${WORKSPACE} || ! ${VERSION} ]]; then
 fi
 
 echo "----------------------------------------"
-echo "Packaging ${OS} Build"
+echo "Packaging Build for ${OS}"
 echo "----------------------------------------"
 
 mkdir -p ${WORKSPACE}/release
 
 if [[ ${OS} == "windows" ]]; then
     STRIPPROG="/usr/bin/x86_64-w64-mingw32-strip"
-    ${STRIPPROG} ./src/cmusicaid.exe
-    ${STRIPPROG} ./src/cmusicai-cli.exe
-    ${STRIPPROG} ./src/qt/cmusicai-qt.exe
-    install -c -s ./src/cmusicaid.exe ${WORKSPACE}/release
-    install -c -s ./src/cmusicai-cli.exe ${WORKSPACE}/release
-    install -c -s ./src/qt/cmusicai-qt.exe ${WORKSPACE}/release
-
-    # Create a zip package
+    ${STRIPPROG} ${WORKSPACE}/src/cmusicaid.exe
+    ${STRIPPROG} ${WORKSPACE}/src/cmusicai-cli.exe
+    ${STRIPPROG} ${WORKSPACE}/src/qt/cmusicai-qt.exe
+    mv ${WORKSPACE}/src/cmusicaid.exe ${WORKSPACE}/release
+    mv ${WORKSPACE}/src/cmusicai-cli.exe ${WORKSPACE}/release
+    mv ${WORKSPACE}/src/qt/cmusicai-qt.exe ${WORKSPACE}/release
     cd ${WORKSPACE}/release
-    zip -r cmusicai-${VERSION}-win64.zip *
-    mv ${WORKSPACE}/release/cmusicai-${VERSION}-win64.zip ${WORKSPACE}/release/cmusicai-${VERSION}-win64-setup.zip
-
-elif [[ ${OS} == "linux" || ${OS} == "arm32v7" || ${OS} == "aarch64" ]]; then
-    STRIPPROG="/usr/bin/strip"
-    ${STRIPPROG} ./src/cmusicaid
-    ${STRIPPROG} ./src/cmusicai-cli
-    install -c -s ./src/cmusicaid ${WORKSPACE}/release
-    install -c -s ./src/cmusicai-cli ${WORKSPACE}/release
-
-    # Create a tar package
-    cd ${WORKSPACE}/release
-    tar -czvf cmusicai-${VERSION}-${OS}.tar.gz *
-    mv ${WORKSPACE}/release/cmusicai-${VERSION}-${OS}.tar.gz ${WORKSPACE}/release/cmusicai-${VERSION}-${OS}-setup.tar.gz
-
+    zip -r ${WORKSPACE}/release/cmusicai-${VERSION}-win64.zip .
+elif [[ ${OS} == "linux" || ${OS} == "aarch64" || ${OS} == "arm32v7" ]]; then
+    cd ${WORKSPACE}/src
+    tar -czvf ${WORKSPACE}/release/cmusicai-${VERSION}-${OS}.tar.gz cmusicaid cmusicai-cli qt/cmusicai-qt
 else
-    echo "Unsupported OS: ${OS}"
+    echo "Unsupported OS"
     exit 1
 fi
-
-echo "Packaging Complete"
