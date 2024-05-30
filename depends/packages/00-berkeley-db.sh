b9f620cde5417ae3e72f7171fc1faa7a4735262f
Barrett Patterson <barrett@poolspots.com>
2024-05-30 00:12:47 -0400

    Update 00-berkeley-db.sh

--- depends/packages/00-berkeley-db.sh
+++ depends/packages/00-berkeley-db.sh
@@ -1,4 +1,3 @@
-# 00-berkeley-db.sh
 #!/usr/bin/env bash
 
 OS=${1}
@@ -7,12 +6,32 @@ if [[ ${OS} == "osx" ]]; then
     echo "Installing Berkeley DB for macOS"
     mkdir -p depends/SDKs
     cd depends/SDKs
-    curl -O http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+    curl -L --retry 5 --retry-delay 5 --connect-timeout 10 --max-time 300 -O http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "Error downloading Berkeley DB"
+        exit 1
+    fi
     tar -zxvf db-4.8.30.NC.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "Error extracting Berkeley DB"
+        exit 1
+    fi
     cd db-4.8.30.NC/build_unix
     ../dist/configure --prefix=$PWD/../..
+    if [ $? -ne 0 ]; then
+        echo "Error configuring Berkeley DB"
+        exit 1
+    fi
     make -j$(nproc)
+    if [ $? -ne 0 ]; then
+        echo "Error building Berkeley DB"
+        exit 1
+    fi
     make install
+    if [ $? -ne 0 ]; then
+        echo "Error installing Berkeley DB"
+        exit 1
+    fi
 else
     echo "Skipping Berkeley DB installation for ${OS}"
 fi
