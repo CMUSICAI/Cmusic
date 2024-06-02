bb7d934ae27c32556357655d3982719ae4fa2bfa
Barrett Patterson <barrett@poolspots.com>
2024-06-02 15:05:04 -0400

    Update 02-copy-build-dependencies.sh

--- .github/scripts/02-copy-build-dependencies.sh
+++ .github/scripts/02-copy-build-dependencies.sh
@@ -9,12 +9,13 @@ if [[ ! ${OS} || ! ${GITHUB_WORKSPACE} ]]; then
     echo "Usage: ${0} <operating system> <github workspace path>"
     exit 1
 fi
+
 echo "----------------------------------------"
 echo "OS: ${OS}"
 echo "----------------------------------------"
 
 if [[ ${OS} == "arm32v7-disable-wallet" || ${OS} == "linux-disable-wallet" || ${OS} == "aarch64-disable-wallet" ]]; then
-    OS=`echo ${OS} | cut -d"-" -f1`
+    OS=$(echo ${OS} | cut -d"-" -f1)
 fi
 
 echo "----------------------------------------"
@@ -25,7 +26,7 @@ cd depends
 if [[ ${OS} == "windows" ]]; then
     make HOST=x86_64-w64-mingw32 -j2
 elif [[ ${OS} == "osx" ]]; then
-    mkdir SDKs
+    mkdir -p SDKs
     cd SDKs
     curl -O https://bitcoincore.org/depends-sources/sdks/Xcode-11.3.1-11C505-extracted-SDK-with-libcxx-headers.tar.gz
     tar -zxf Xcode-11.3.1-11C505-extracted-SDK-with-libcxx-headers.tar.gz
@@ -33,6 +34,16 @@ elif [[ ${OS} == "osx" ]]; then
     cd ..
     chmod +x contrib/install_db4.sh
     ./contrib/install_db4.sh $(pwd)
+
+    export BDB_PREFIX=${GITHUB_WORKSPACE}/db4
+    export BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8"
+    export BDB_CFLAGS="-I${BDB_PREFIX}/include"
+    export BOOST_ROOT=/usr/include/boost
+    export BOOST_LIB_PATH=/usr/lib/x86_64-linux-gnu
+
+    CONFIG_SITE=${GITHUB_WORKSPACE}/depends/x86_64-apple-darwin14/share/config.site \
+    ./configure --prefix=/ --disable-ccache --disable-maintainer-mode --disable-dependency-tracking --enable-reduce-exports --disable-bench --with-gui=qt5 --with-boost=${BOOST_ROOT} --with-boost-libdir=${BOOST_LIB_PATH} GENISOIMAGE=${GITHUB_WORKSPACE}/depends/x86_64-apple-darwin14/native/bin/genisoimage BDB_LIBS="${BDB_LIBS}" BDB_CFLAGS="${BDB_CFLAGS}"
+
     make HOST=x86_64-apple-darwin14 -j2
 elif [[ ${OS} == "linux" || ${OS} == "linux-disable-wallet" ]]; then
     make HOST=x86_64-linux-gnu -j2
